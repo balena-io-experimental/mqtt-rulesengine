749abd1fc563a71828a2883fabf1208fad47141e
John (Jack) Brown <jack@brownjohnf.com>
2017-04-15 11:40:55 -0700

    Convert to rules engine

--- default.js
+++ default.js
@@ -2,25 +2,20 @@
 
 const
   _       = require('lodash'),
+  debug   = require('debug')('mqtt-rebroadcast'),
   express = require('express'),
   app     = express(),
   server  = require('http').createServer(app),
   mqtt    = require('mqtt'),
-  io      = require('socket.io')(server),
   redis = require("redis")
 
 // set vars from env
 const
-  mqttServer  = process.env.MQTT_SERVER,
-  mqttTopic   = process.env.MQTT_TOPIC,
-  redisUrl    = process.env.REDIS_URL,
+  mqttServer    = process.env.MQTT_SERVER,
+  mqttTopic     = process.env.MQTT_TOPIC,
+  redisUrl      = process.env.REDIS_URL,
   dashboardPort = process.env.PORT,
-  devices     = _.fromPairs(_.map(_.split(process.env.DEVICES, ','), pair => {
-    return _.split(pair, '=')
-  })),
-  republish   = !(process.env.DRY_RUN == 1)
-
-console.log('devices: ', devices)
+  republish     = !(process.env.DRY_RUN == 1)
 
 // set up connections
 const
@@ -89,23 +84,23 @@ const handlers = {
 
 const publishToMqtt = (channel, room, data, ...suffixes) => {
   const topic = _.join(_.flatten([channel, room, suffixes]), '/')
+  const message = JSON.stringify(data)
 
-  if (republish) {
-    return mqttClient.publish(topic, JSON.stringify(data))
-  } else {
+  debug('   --> ', topic, message)
+  if (!republish) {
     return
   }
+
+  return mqttClient.publish(topic, message)
 }
 
 redisClient.on("error", (err) => {
   console.log("Error " + err)
 })
 
-app.use(express.static(__dirname))
-
 mqttClient.on('connect', () => {
-  mqttClient.subscribe(mqttTopic),
-  mqttClient.publish("Wxec0cXgwgC9KwBK", "node app connected")
+  mqttClient.subscribe(mqttTopic + '/#'),
+  mqttClient.publish("Wxec0cXgwgC9KwBK", "mqtt rules engine/rebroadcast connected")
 })
 
 app.get('/', (req, res) => {
@@ -131,6 +126,7 @@ app.get('/data/:room/:deviceId', (req, res) => {
 
 mqttClient.on('message', (topic, message) => {
   // message is Buffer
+  debug('<--    ', topic, message.toString())
 
   const [channel, room, ...rest]  = _.split(topic, '/')
 
@@ -141,61 +137,6 @@ mqttClient.on('message', (topic, message) => {
     handler(channel, room, message.toString(), topic, ...rest)
   })
 })
-
-io.on('connection', function (socket) {
-  console.log('socket.io connection open')
-
-  if (handlers.temperature == null) {
-    _.set(handlers, 'temperature', [])
-  }
-
-  if (handlers.humidity == null) {
-    _.set(handlers, 'humidity', [])
-  }
-
-  if (handlers.events == null) {
-    _.set(handlers, 'events', [])
-  }
-
-  if (handlers.lightlevel == null) {
-    _.set(handlers, 'lightlevel', [])
-  }
-
-  handlers.lightlevel.push((channel, room, message) => {
-    const data = JSON.parse(message)
-    console.log(data)
-    data.device.name = _.get(devices, data.device.id, 'unknown')
-    socket.emit('updateChart', data)
-  })
-
-  handlers.temperature.push((channel, room, message) => {
-    const data = JSON.parse(message)
-    socket.emit('updateChart', _.merge(data, {
-      device: {
-        name: _.get(devices, data.device.id, 'unknown')
-      }
-    }))
-  })
-
-  handlers.humidity.push((channel, room, message) => {
-    const data = JSON.parse(message)
-    socket.emit('updateChart', _.merge(data, {
-      device: {
-        name: _.get(devices, data.device.id, 'unknown')
-      }
-    }))
-  })
-
-  handlers.events.push((channel, room, message) => {
-    console.log('adding motion event')
-    const data = JSON.parse(message)
-    socket.emit('addEvent', _.merge(data, {
-      device: {
-        name: _.get(devices, data.device.id, 'unknown')
-      }
-    }))
-  })
-});
 
 server.listen(dashboardPort)
 
